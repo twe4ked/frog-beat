7b6243e7e7f7b7b261d96f302259da219807977c
Odin Dutton <odindutton@gmail.com>
2019-09-09 12:39:17 +1000

    Introduce system module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use coffee::graphics::{Color, Frame, Mesh, Rectangle, Shape, Window, WindowSettings};
-use coffee::input::keyboard::KeyCode;
+mod system;
+
+use coffee::graphics::{Color, Frame, Mesh, Window, WindowSettings};
 use coffee::input::KeyboardAndMouse;
 use coffee::load::loading_screen::ProgressBar;
 use coffee::load::Task;
 use coffee::{Game, Result, Timer};
-use legion::query::{IntoQuery, Query, Read, Write};
 use legion::{Universe, World};
 
 const PRUSSIAN_BLUE: Color = Color {
@@ -113,73 +113,19 @@ impl Game for FrogBeat {
     }
 
     fn interact(&mut self, input: &mut Self::Input, _window: &mut Window) {
-        let query = <(Write<Player>)>::query();
-        for player in query.iter(&self.world) {
-            if input.was_key_released(KeyCode::W) {
-                player.direction = Some(Direction::Up);
-            }
-            if input.was_key_released(KeyCode::S) {
-                player.direction = Some(Direction::Down);
-            }
-            if input.was_key_released(KeyCode::A) {
-                player.direction = Some(Direction::Left);
-            }
-            if input.was_key_released(KeyCode::D) {
-                player.direction = Some(Direction::Right);
-            }
-        }
+        system::interact(&mut self.world, input);
     }
 
     fn update(&mut self, _window: &Window) {
-        let query = <(Write<Player>, Write<TilePosition>)>::query();
-        for (player, tile_position) in query.iter(&self.world) {
-            match &player.direction {
-                Some(direction) => {
-                    match direction {
-                        Direction::Up => tile_position.y -= 1.0,
-                        Direction::Down => tile_position.y += 1.0,
-                        Direction::Left => tile_position.x -= 1.0,
-                        Direction::Right => tile_position.x += 1.0,
-                    }
-                    player.direction = None;
-                }
-                None => {}
-            }
-        }
-
-        let query = <(Write<Position>, Read<TilePosition>)>::query();
-        for (position, tile_position) in query.iter(&self.world) {
-            if position.y > tile_position.y * TILE_SIZE {
-                position.y -= 10.0;
-            }
-            if position.y < tile_position.y * TILE_SIZE {
-                position.y += 10.0;
-            }
-            if position.x < tile_position.x * TILE_SIZE {
-                position.x += 10.0;
-            }
-            if position.x > tile_position.x * TILE_SIZE {
-                position.x -= 10.0;
-            }
-        }
+        system::update_player_tile_position(&mut self.world);
+        system::update_position(&mut self.world);
     }
 
     fn draw(&mut self, frame: &mut Frame, _timer: &Timer) {
         frame.clear(Color::BLACK);
 
         let mut mesh = Mesh::new();
-
-        let query = <(Read<Position>, Read<Color>)>::query();
-        for (position, color) in query.iter(&self.world) {
-            let rect = Rectangle {
-                x: position.x,
-                y: position.y,
-                width: TILE_SIZE,
-                height: TILE_SIZE,
-            };
-            mesh.fill(Shape::Rectangle(rect), *color);
-        }
-
+        system::draw_entities(&self.world, &mut mesh);
         mesh.draw(&mut frame.as_target());
     }
 }
---
+++ src/system.rs
@@ -0,0 +1,73 @@
+use super::{Direction, Player, Position, TilePosition, TILE_SIZE};
+use coffee::graphics::{Color, Mesh, Rectangle, Shape};
+use coffee::input::keyboard::KeyCode;
+use coffee::input::KeyboardAndMouse;
+use legion::query::{IntoQuery, Query, Read, Write};
+use legion::World;
+
+pub fn interact(world: &mut World, input: &mut KeyboardAndMouse) {
+    let query = <(Write<Player>)>::query();
+    for player in query.iter(world) {
+        if input.was_key_released(KeyCode::W) {
+            player.direction = Some(Direction::Up);
+        }
+        if input.was_key_released(KeyCode::S) {
+            player.direction = Some(Direction::Down);
+        }
+        if input.was_key_released(KeyCode::A) {
+            player.direction = Some(Direction::Left);
+        }
+        if input.was_key_released(KeyCode::D) {
+            player.direction = Some(Direction::Right);
+        }
+    }
+}
+
+pub fn update_player_tile_position(world: &mut World) {
+    let query = <(Write<Player>, Write<TilePosition>)>::query();
+    for (player, tile_position) in query.iter(world) {
+        match &player.direction {
+            Some(direction) => {
+                match direction {
+                    Direction::Up => tile_position.y -= 1.0,
+                    Direction::Down => tile_position.y += 1.0,
+                    Direction::Left => tile_position.x -= 1.0,
+                    Direction::Right => tile_position.x += 1.0,
+                }
+                player.direction = None;
+            }
+            None => {}
+        }
+    }
+}
+
+pub fn update_position(world: &mut World) {
+    let query = <(Write<Position>, Read<TilePosition>)>::query();
+    for (position, tile_position) in query.iter(world) {
+        if position.y > tile_position.y * TILE_SIZE {
+            position.y -= 10.0;
+        }
+        if position.y < tile_position.y * TILE_SIZE {
+            position.y += 10.0;
+        }
+        if position.x < tile_position.x * TILE_SIZE {
+            position.x += 10.0;
+        }
+        if position.x > tile_position.x * TILE_SIZE {
+            position.x -= 10.0;
+        }
+    }
+}
+
+pub fn draw_entities(world: &World, mesh: &mut Mesh) {
+    let query = <(Read<Position>, Read<Color>)>::query();
+    for (position, color) in query.iter(world) {
+        let rect = Rectangle {
+            x: position.x,
+            y: position.y,
+            width: TILE_SIZE,
+            height: TILE_SIZE,
+        };
+        mesh.fill(Shape::Rectangle(rect), *color);
+    }
+}
